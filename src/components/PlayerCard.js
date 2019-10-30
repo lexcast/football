235eb5153508353fb10c8bb39f28deab378af75b
Daniel Alejandro Cast <lexcast13@gmail.com>
2019-10-30 14:24:55 -0600

    Remove gyroscope

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -4,7 +4,10 @@ import Card from "./Card";
 
 const PlayerCard = props => {
   return (
-    <Tilt className="w-48 h-64" options={{ max: 10, speed: 300, scale: 1.05 }}>
+    <Tilt
+      className="w-48 h-64"
+      options={{ max: 10, speed: 300, scale: 1.05, gyroscope: false }}
+    >
       <Card>
         <div key="FRONT" className="bg-orange-500">
           FRONT
