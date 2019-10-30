d5ef70216a4362ceb0bb38b85d778240832da28f
Daniel Alejandro Cast <lexcast13@gmail.com>
2019-10-30 13:55:11 -0600

    Add initial PlayerCard component

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,12 @@
 import React from "react";
-import logo from "assets/logo.svg";
+import PlayerCard from "./PlayerCard";
 
 const App = () => {
   return (
-    <div className="font-sans text-center">
-      <header
-        className="bg-gray-800 min-h-screen flex flex-col items-center justify-center text-white"
-        style={{ fontSize: "calc(10px + 2vmin)" }}
-      >
-        <img src={logo} style={{ height: "40vmin" }} alt="logo" />
-        <p>
-          Edit <code className="font-mono">src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="text-teal-500"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen flex bg-gray-100 p-10">
+      <PlayerCard />
     </div>
   );
 };
 
 export default App;
-
-// Grid
-// Card
-// Card front
-// Card back
-// Flip on click
-// 3D hover
---
+++ src/components/PlayerCard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Tilt from "./Tilt";
+import Card from "./Card";
+
+const PlayerCard = props => {
+  return (
+    <Tilt className="w-48 h-64" options={{ max: 10, speed: 300, scale: 1.05 }}>
+      <Card>
+        <div key="FRONT" className="bg-orange-500">
+          FRONT
+        </div>
+        <div key="BACK" className="bg-green-800">
+          BACK
+        </div>
+      </Card>
+    </Tilt>
+  );
+};
+
+export default React.memo(PlayerCard);
