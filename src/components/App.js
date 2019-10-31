c9c4454256f43e05d5e210e70a1a0866efa36df0
Daniel Alejandro Cast <lexcast13@gmail.com>
2019-10-30 20:21:03 -0600

    Add initial data, and add grid selector

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import PlayerCard from "./PlayerCard";
+import data from "data";
 
 const App = () => {
+  const [grid, setGrid] = useState(Object.keys(data.grids)[0]);
+
   return (
-    <div className="w-screen h-screen flex bg-gray-100 p-10">
-      <PlayerCard />
+    <div className="w-screen h-screen bg-gray-200">
+      <div className="px-10 py-5">
+        {Object.keys(data.grids).map(key => {
+          const g = data.grids[key];
+          return (
+            <div
+              key={key}
+              onClick={() => setGrid(key)}
+              className={
+                "inline-flex items-center mr-2 py-2 px-3 border-2 cursor-pointer rounded-full bg-gray-300 text-gray-800 text-sm font-semibold " +
+                (grid === key
+                  ? "border-teal-400"
+                  : "border-transparent hover:bg-gray-400")
+              }
+            >
+              <img src={g.logo} alt={g.title} className="h-6 mr-1" />
+              {g.title}
+            </div>
+          );
+        })}
+      </div>
+      <div className="px-10 py-5">
+        {data.grids[grid].players.map(key => {
+          return (
+            <PlayerCard
+              key={key}
+              player={data.players[key]}
+              grid={data.grids[grid]}
+              nationalTeams={data.nationalTeams}
+              clubs={data.clubs}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
