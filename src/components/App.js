80deb7693149a871e22e60da32deb1a186fbf014
Daniel Alejandro Cast <lexcast13@gmail.com>
2019-11-18 01:55:30 -0600

    Sort teams by country

--- src/components/App.js
+++ src/components/App.js
@@ -9,27 +9,35 @@ const App = () => {
   return (
     <div className="w-full h-full">
       <div className="px-6 py-5 flex flex-wrap">
-        {Object.keys(data.clubs).map(key => {
-          const t = data.clubs[key];
-          return (
-            <div
-              key={key}
-              onClick={() => setTeam(team === key ? null : key)}
-              className={
-                "inline-flex items-center ml-1 mb-1 w-12 h-12 p-2 border-2 cursor-pointer rounded-full bg-gray-300 " +
-                (team === key
-                  ? "border-teal-400"
-                  : "border-transparent hover:bg-gray-400")
-              }
-            >
-              <img
-                src={getLogo(key)}
-                alt={t.title}
-                className="max-h-full max-w-full mx-auto"
-              />
-            </div>
-          );
-        })}
+        {Object.keys(data.clubs)
+          .map(key => data.clubs[key])
+          .sort((a, b) => {
+            const c1 = (a.country || "1") + a.title;
+            const c2 = (b.country || "1") + b.title;
+
+            return c1 > c2 ? 1 : c1 < c2 ? -1 : 0;
+          })
+          .map(t => {
+            return (
+              <div
+                key={t.id}
+                title={t.title}
+                onClick={() => setTeam(team === t.id ? null : t.id)}
+                className={
+                  "inline-flex items-center ml-1 mb-1 w-12 h-12 p-2 border-2 cursor-pointer rounded-full bg-gray-300 " +
+                  (team === t.id
+                    ? "border-teal-400"
+                    : "border-transparent hover:bg-gray-400")
+                }
+              >
+                <img
+                  src={getLogo(t.id)}
+                  alt={t.title}
+                  className="max-h-full max-w-full mx-auto"
+                />
+              </div>
+            );
+          })}
       </div>
       <div className="px-6 py-5 flex flex-wrap justify-center">
         {Object.keys(data.players)
@@ -39,13 +47,12 @@ const App = () => {
 
             return player;
           })
-          .filter(player => {
-            return (
+          .filter(
+            player =>
               !team ||
               player.clubs.includes(team) ||
               player.nationalTeam === team
-            );
-          })
+          )
           .sort((a, b) => {
             const pa = a.name.split(" ");
             const pb = b.name.split(" ");
