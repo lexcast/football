de33f6bc737ba8c523cc2dd3d590cda7d5f140dc
Daniel Alejandro Cast <lexcast13@gmail.com>
2019-11-20 13:09:32 -0700

    Add empty grid message

--- src/components/PlayersGrid.js
+++ src/components/PlayersGrid.js
@@ -3,8 +3,43 @@ import PlayerCard from "./PlayerCard";
 import { positions } from "./PositionFilter";
 
 const PlayersGrid = ({ search, team, position, players, clubs }) => {
+  const playersFiltered = Object.keys(players)
+    .map(key => {
+      const player = players[key];
+      player.id = key;
+
+      return player;
+    })
+    .filter(
+      player =>
+        !position ||
+        player.position === position ||
+        (positions[position] && positions[position].includes(player.position))
+    )
+    .filter(
+      player =>
+        !team || player.clubs.includes(team) || player.nationalTeam === team
+    )
+    .filter(
+      player =>
+        !search ||
+        player.id.includes(search.toLowerCase().replace(" ", "")) ||
+        player.name.toLowerCase().includes(search.toLowerCase()) ||
+        player.number === parseInt(search)
+    )
+    .sort((a, b) => {
+      const pa = a.name.split(" ");
+      const pb = b.name.split(" ");
+
+      return pa[pa.length - 1] > pb[pb.length - 1]
+        ? 1
+        : pa[pa.length - 1] < pb[pb.length - 1]
+        ? -1
+        : 0;
+    });
+
   return (
-    <div class="bg-green-500 px-2 py-4">
+    <div className="bg-green-500 px-2 py-4">
       <div
         style={{ minHeight: "250px" }}
         className="border-4 border-white px-6 py-5 flex flex-wrap justify-center relative overflow-hidden"
@@ -14,44 +49,8 @@ const PlayersGrid = ({ search, team, position, players, clubs }) => {
         <div className="w-8 h-8 rounded-full border-4 border-white absolute bottom-0 left-0 -ml-4 -mb-4"></div>
         <div className="w-8 h-8 rounded-full border-4 border-white absolute bottom-0 right-0 -mr-4 -mb-4"></div>
 
-        {Object.keys(players)
-          .map(key => {
-            const player = players[key];
-            player.id = key;
-
-            return player;
-          })
-          .filter(
-            player =>
-              !position ||
-              player.position === position ||
-              (positions[position] &&
-                positions[position].includes(player.position))
-          )
-          .filter(
-            player =>
-              !team ||
-              player.clubs.includes(team) ||
-              player.nationalTeam === team
-          )
-          .filter(
-            player =>
-              !search ||
-              player.id.includes(search.toLowerCase().replace(" ", "")) ||
-              player.name.toLowerCase().includes(search.toLowerCase()) ||
-              player.number === parseInt(search)
-          )
-          .sort((a, b) => {
-            const pa = a.name.split(" ");
-            const pb = b.name.split(" ");
-
-            return pa[pa.length - 1] > pb[pb.length - 1]
-              ? 1
-              : pa[pa.length - 1] < pb[pb.length - 1]
-              ? -1
-              : 0;
-          })
-          .map(player => {
+        {playersFiltered.length > 0 ? (
+          playersFiltered.map(player => {
             return (
               <PlayerCard
                 key={player.id}
@@ -61,7 +60,15 @@ const PlayersGrid = ({ search, team, position, players, clubs }) => {
                 clubs={clubs}
               />
             );
-          })}
+          })
+        ) : (
+          <div className="text-center flex-wrap text-green-400 flex flex-col justify-center items-center font-bungee font-bold">
+            <div className="text-5xl leading-none">No players found</div>
+            <div className="text-lg">
+              Change filters criteria for other results
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
