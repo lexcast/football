c1f6d14ec75f92e6595f68b7c6d377c31f0dc10e
Daniel Alejandro Cast <lexcast13@gmail.com>
2020-05-25 22:44:56 -0500

    Updates

---
+++ precache-manifest.c1718dc1cd54aea2935ce7c3c8e94d88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac45f24cd71bfae181ab14e2b88e709",
+    "url": "/football/index.html"
+  },
+  {
+    "revision": "54173399cb20bcd63005",
+    "url": "/football/static/css/main.dae376fc.chunk.css"
+  },
+  {
+    "revision": "33cc2ca04baabf126ad0",
+    "url": "/football/static/js/2.0e7493c4.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "/football/static/js/2.0e7493c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54173399cb20bcd63005",
+    "url": "/football/static/js/main.151b03d1.chunk.js"
+  },
+  {
+    "revision": "627fc599bc6dd610c9fd",
+    "url": "/football/static/js/runtime-main.b23e0173.js"
+  },
+  {
+    "revision": "d9c7b697b223714dae8b270a9b949c7d",
+    "url": "/football/static/media/tie.d9c7b697.svg"
+  }
+]);
