47007eb7594945bc1e34c5cad1b33cd03ac7724b
Daniel Alejandro Cast <lexcast13@gmail.com>
2020-05-25 22:33:56 -0500

    Add gh-pages & fix images urls

--- src/utils/image.js
+++ src/utils/image.js
@@ -1,10 +1,13 @@
-const getLogo = teamId => `/images/logos/${teamId}.png`;
+const getLogo = (teamId) =>
+  `${process.env.PUBLIC_URL}/images/logos/${teamId}.png`;
 
 const getPlayer = (playerId, teamId) =>
-  `/images/players/${playerId}/${teamId}.png`;
+  `${process.env.PUBLIC_URL}/images/players/${playerId}/${teamId}.png`;
 
-const getSignature = playerId => `/images/signatures/${playerId}.png`;
+const getSignature = (playerId) =>
+  `${process.env.PUBLIC_URL}/images/signatures/${playerId}.png`;
 
-const getBrand = brandId => `/images/brands/${brandId}.svg`;
+const getBrand = (brandId) =>
+  `${process.env.PUBLIC_URL}/images/brands/${brandId}.svg`;
 
 export { getLogo, getPlayer, getSignature, getBrand };
